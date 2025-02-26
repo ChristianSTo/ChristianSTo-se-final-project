509483648d076e7529146be76ccc4ade4146e218
ChristianTo <christian.to@outlook.com>
2025-02-26 15:59:47 -0600

    namechange

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ChristianSTo-se_final_project/",
+  base: "/ChristianSTo-se-final-project/",
 });
